750fc6a263bfdcc6ef31a9f56c36503253400266
abadon.gutierrez <abadon.gutierrez@gmail.com>
2024-05-01 22:52:52 -0600

    using kotlin 1.9.23 and Java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "1.9.23"
     application
 }
 
@@ -20,7 +20,7 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(21)
 }
 
 application {
